6c88dd82040eba38f602e3b0f43dfe23947cb93e
Ademola Madelewi <ademolamadelewi@gmail.com>
2021-01-10 23:09:34 +0100

    change redirect uri

--- src/HTTP/keys.js
+++ src/HTTP/keys.js
@@ -1,9 +1,9 @@
 let keys = {};
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.REACT_APP_NODE_ENV === "production") {
   keys = {
-    token: process.env.TOKEN,
-    clientID: process.env.CLIENT_ID,
+    token: process.env.REACT_APP_TOKEN,
+    clientID: process.env.REACT_APP_CLIENT_ID,
   };
 } else {
   keys = require("./devKeys").default;
